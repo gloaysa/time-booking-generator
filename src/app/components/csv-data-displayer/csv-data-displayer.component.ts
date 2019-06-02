eb412c8b733947fb05ade7778666af1f7f4f6e60
Guillermo Loaysa <gloaysa@gmail.com>
2019-06-02 19:05:05 +0200

    add style and create table

--- src/app/components/csv-data-displayer/csv-data-displayer.component.ts
+++ src/app/components/csv-data-displayer/csv-data-displayer.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {PapaparseService} from '../../services/papaparse.service';
 import { saveAs } from 'file-saver';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-csv-data-displayer',
@@ -10,21 +11,37 @@ import { saveAs } from 'file-saver';
 })
 export class CsvDataDisplayerComponent implements OnInit, OnDestroy {
 
-  constructor(private papaService: PapaparseService) { }
+  constructor(
+    private papaService: PapaparseService,
+    private router: Router
+  ) { }
 
   csvData: string[][];
   getDataSubscription;
 
   ngOnInit() {
     this.getDataSubscription = this.papaService.getCsvData()
-      .subscribe(csvData => this.csvData = csvData);
+      .subscribe(csvData => {
+        this.csvData = csvData;
+        console.log(this.csvData);
+      });
   }
 
   downloadCSV() {
     const blob = new Blob([this.parseJSONtoCSV()], { type: 'text/csv' });
     const title = 'Time_Bookings_' + new Date().toLocaleDateString();
     saveAs(blob, title + '.csv');
+    this.router.navigateByUrl('/');
+  }
 
+  getTableHeaders() {
+    return this.csvData[0];
+  }
+
+  getTableBody() {
+    return this.csvData.filter((rows, index) => {
+      return index !== 0;
+    });
   }
 
   parseJSONtoCSV() {
