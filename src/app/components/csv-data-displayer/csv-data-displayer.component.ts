6578aa75f4ca82ca678a9dbf158774d673a70dff
Guillermo Loaysa <gloaysa@gmail.com>
2019-06-02 16:25:47 +0200

    add file saver

--- src/app/components/csv-data-displayer/csv-data-displayer.component.ts
+++ src/app/components/csv-data-displayer/csv-data-displayer.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {PapaparseService} from '../../services/papaparse.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-csv-data-displayer',
@@ -17,6 +18,17 @@ export class CsvDataDisplayerComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.getDataSubscription = this.papaService.getCsvData()
       .subscribe(csvData => this.csvData = csvData);
+  }
+
+  downloadCSV() {
+    const blob = new Blob([this.parseJSONtoCSV()], { type: 'text/csv' });
+    const title = 'Time_Bookings_' + new Date().toLocaleDateString();
+    saveAs(blob, title + '.csv');
+
+  }
+
+  parseJSONtoCSV() {
+    return this.papaService.unparseData(this.csvData);
   }
 
   ngOnDestroy(): void {
